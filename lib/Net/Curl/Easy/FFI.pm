bac7e111e85bc2df8efcd6af1ed6ffbccfacd8c9
Graham Ollis <plicease@cpan.org>
2022-09-26 21:41:02 -0600

    Initial structure

---
+++ lib/Net/Curl/Easy/FFI.pm
@@ -0,0 +1,12 @@
+package Net::Curl::Easy::FFI;
+
+use strict;
+use warnings;
+use 5.008004;
+
+# ABSTRACT: Perl interface to libcurl's "easy" interface
+# VERSION
+
+1;
+
+
---
+++ t/net_curl_easy_ffi.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use Net::Curl::Easy::FFI;
+
+ok 1, 'todo';
+
+done_testing;
+
+

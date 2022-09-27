0e8e902fd5676ee8748e2e138649997d9b7a57f7
Graham Ollis <plicease@cpan.org>
2022-09-27 17:25:58 -0600

    generate getinfos

--- maint/ref-update.pl
+++ maint/ref-update.pl
@@ -13,27 +13,42 @@ require Net::Swirl::CurlEasy;
 
 my $curl_h = path('/usr/include/x86_64-linux-gnu/curl/curl.h');
 my @options;
+my @info;
 my %missing;
 my $total = 0;
 my $missing = 0;
 
-my %option_init = do {
+my %option_init;
+my %info_init;
+
+{
   my $castxml = Clang::CastXML->new;
   my $header = $castxml->introspect($curl_h)->to_href;
 
-  # convert into a hash, remove CURLOPT_ prefix for name
-  map { $_->{name} =~ s/^CURLOPT_//r => $_->{init} }
-  # get the inner list of the enum
-  map { $_->{inner}->@* }
-  # only consider objects called CURLoption (the enum), that have an inner list
-  grep { defined $_->{name} && $_->{name} eq 'CURLoption' && defined $_->{inner} }
-  # get all of the items in the header
-  $header->{inner}->@*
+  %option_init = 
+    # convert into a hash, remove CURLOPT_ prefix for name
+    map { $_->{name} =~ s/^CURLOPT_//r => $_->{init} }
+    # get the inner list of the enum
+    map { $_->{inner}->@* }
+    # only consider objects called CURLoption (the enum), that have an inner list
+    grep { defined $_->{name} && $_->{name} eq 'CURLoption' && defined $_->{inner} }
+    # get all of the items in the header
+    $header->{inner}->@*;
+
+  %info_init = 
+    # convert into a hash, remove CURLOPT_ prefix for name
+    map { $_->{name} =~ s/^CURLINFO_//r => $_->{init} }
+    # get the inner list of the enum
+    map { $_->{inner}->@* }
+    # only consider objects called CURLoption (the enum), that have an inner list
+    grep { defined $_->{name} && $_->{name} eq 'CURLINFO' && defined $_->{inner} }
+    # get all of the items in the header
+    $header->{inner}->@*;
 };
 
 foreach my $line ($curl_h->lines)
 {
-  if($line =~ /CURLOPT\(\s*CURLOPT_(\S*)\s*,\s*CURLOPTTYPE_(\S+)\s*,\s*\S+\s*\)/)
+  if($line =~ /CURLOPT\(\s*CURLOPT_(\S+)\s*,\s*CURLOPTTYPE_(\S+)\s*,\s*\S+\s*\)/)
   {
     my $name = $1;
     my $type = $2;
@@ -52,7 +67,6 @@ foreach my $line ($curl_h->lines)
     {
       push @options, {
         perl_name => lc $name,
-        #c_name    => "CURLOPT_$name",
         xsub_name => "_setopt_@{[ lc $type ]}",
         init      => $init,
       };
@@ -60,6 +74,31 @@ foreach my $line ($curl_h->lines)
     else
     {
       push $missing{options}->{$type}->@*, "$name:$init";
+      $missing++;
+    }
+
+  }
+
+  if($line =~ /CURLINFO_(\S+)\s+=\s+CURLINFO_(\S+)/)
+  {
+    my $name = $1;
+    my $type = $2;
+
+    my $init = delete $info_init{$name};
+
+    $total++;
+
+    if($type =~ /^(STRING|DOUBLE|LONG|OFF_T)$/ && $init)
+    {
+      push @info, {
+        perl_name => lc $name,
+        xsub_name => "_getinfo_@{[ lc $type ]}",
+        init      => $init,
+      };
+    }
+    else
+    {
+      push $missing{info}->{$type}->@*, "$name:$init";
       $missing++;
     }
 
@@ -77,12 +116,15 @@ my $tt = Template->new({
   },
 });
 
-$tt->process('Option.pm.tt', {
-  curl => { options => \@options },
-}, "lib/Net/Swirl/CurlEasy/Options.pm" ) or do {
-  say "Error generating lib/Net/Swirl/CurlEasy/Options.pm @{[ $tt->error ]}";
-  exit 2;
-};
+foreach my $name (qw( Options Info ))
+{
+  $tt->process("$name.pm.tt", {
+    curl => { options => \@options, infos => \@info  },
+  }, "lib/Net/Swirl/CurlEasy/$name.pm" ) or do {
+    say "Error generating lib/Net/Swirl/CurlEasy/$name.pm @{[ $tt->error ]}";
+    exit 2;
+  };
+}
 
 
 push $missing{options}->{UNKNOWN}->@*, sort keys %option_init;

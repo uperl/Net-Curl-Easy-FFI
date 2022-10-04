6ce62b0da99d3a9d9bb81db87807ef681d96e1b8
Graham Ollis <plicease@cpan.org>
2022-10-04 06:26:46 -0600

    add mutal TLS/SSL to example

--- examples/simplessl.pl
+++ examples/simplessl.pl
@@ -4,10 +4,13 @@ use Net::Swirl::CurlEasy;
 
 my $curl = Net::Swirl::CurlEasy->new;
 
-$curl->setopt(url => 'https://localhost:5001/hello-world')
-     ->setopt(followlocation => 1)
+$curl->setopt(url            => 'https://localhost:5001/hello-world')
      ->setopt(ssl_verifypeer => 1)
-     ->setopt(cainfo => 'examples/tls/Swirl-CA.crt')
+     ->setopt(cainfo         => 'examples/tls/Swirl-CA.crt')
+     ->setopt(sslcert        => 'examples/tls/client.crt')
+     ->setopt(sslkey         => 'examples/tls/client.key')
+     ->setopt(keypasswd      => 'password')
      ->setopt(verbose => 1)
      ->perform;
 
+die "unable to make request" unless $curl->getinfo('response_code') == 200;
